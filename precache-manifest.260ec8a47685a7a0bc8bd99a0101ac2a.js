2aa5be7eab26382db246ed1538df24a27203187f
junecat <junecat@gmail.com>
2020-09-08 15:21:52 +0300

    first

---
+++ precache-manifest.260ec8a47685a7a0bc8bd99a0101ac2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd75b8bc8d31f4c886a34a689ccb92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df2d4cc5e382d7702942",
+    "url": "/static/css/main.87978cef.chunk.css"
+  },
+  {
+    "revision": "c7cb6297023844b28549",
+    "url": "/static/js/2.ad026300.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad026300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2d4cc5e382d7702942",
+    "url": "/static/js/main.0f307fbf.chunk.js"
+  },
+  {
+    "revision": "7aa5be4149651cec6ee9",
+    "url": "/static/js/runtime-main.ead2c0ad.js"
+  }
+]);
